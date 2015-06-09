97718b39daf17505b5de18a0df9124f19f1c427d
kelvinlyan <375898307@qq.com>
2015-06-09 16:48:51 +0800

    up

--- include/database.h
+++
@@ -1,6 +0,0 @@
-#ifndef _COMMON_DATABASE_H
-#define _COMMON_DATABASE_H
-
-#include "database_mongo.h"
-
-#endif
--- include/timer.h
+++ include/timer.h
@@ -1,33 +1,20 @@
-#ifndef _COMMON_TIMER_H
-#define _COMMON_TIMER_H
+#ifndef _TIMER_H
+#define _TIMER_H
 
-#include <sys/time.h>
-#include <stdio.h>
+#include <boost/function.hpp>
 
 class timer
 {
-    public:
-        timer()
-        {
-            gettimeofday(&_begin_time, NULL);
-        }
-        long get_usec()
-        {
-            struct timeval _end_time;
-            gettimeofday(&_end_time, NULL);
-            long diff = _end_time.tv_sec * 1000000 + _end_time.tv_usec - _begin_time.tv_sec * 1000000 - _begin_time.tv_usec;
-            return diff;
-        }
-        double get_sec()
-        {
-            struct timeval _end_time;
-            gettimeofday(&_end_time, NULL);
-            double diff = _end_time.tv_sec - _begin_time.tv_sec + (_end_time.tv_usec - _begin_time.tv_usec) / 1000000.0;
-            return diff;
-        }
-        
-    private:
-        struct timeval _begin_time;
+	public:
+		typedef boost::function<void()> Handler;
+		static int push(unsigned time, Handler h);
+		static void pop(int id);
+		static void run();
+	
+	private:
+		
+
 };
+
 
 #endif
